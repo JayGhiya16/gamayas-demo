006982a5c3356bd679db9f6fd8feed2ec0e9099e
Kiran <Kiran12@gmail.com>
2023-07-07 16:25:29 +0530

    navbar footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,106 @@
+// import React from 'react'
+// import '../styles/Footer.css';
+// import gamay from '../assets/gamay.png'
+// const Footer = () => {
+//   return (
+//     <>
+//     <div className='Main'>
+//       <div className='details'>
+//         <div className='companyDetails'>
+//           <div className='logo'>
+//             <img src={gamay} alt="" />
+//           </div>
+//           <div className='address'>
+//             <p>GAMAYAS Plot No: 1,AP colony, Mallapur, Hyderabad PIN:-500076. <strong>Phone</strong>: +917801073175, +917801073176</p>
+//           </div>
+//         </div>
+
+//         {/* company */}
+//         <div className='company'>
+//           <h4 style={{color: "white"}}>Company</h4>
+//           <ul className='ULlist'>
+//             <li className='list'>
+//               <span>&gt;</span> <a href="/">Home</a>
+//             </li>
+//             <li className='list'>
+//               <span>&gt;</span> <a href="/">About Us</a>
+//             </li>
+//             <li className='list'>
+//               <span>&gt;</span> <a href="/">Portfolio</a>
+//             </li>
+//             <li className='list'>
+//               <span>&gt;</span> <a href="/">Contact us</a>
+//             </li>
+//           </ul>
+//         </div>
+//         {/* Our Focus */}
+//         <div className='company'>
+//           <h4>Our Focus Area</h4>
+//           <ul className='ULlist'>
+//             <li className='list'>
+//               <span>&gt;</span> <a href="/">Web Design</a>
+//             </li>
+//             <li className='list'>
+//               <span>&gt;</span> <a href="/">Web Development</a>
+//             </li>
+//             <li className='list'>
+//               <span>&gt;</span> <a href="/">Mobile Apps Development</a>
+//             </li>
+//             <li className='list'>
+//               <span>&gt;</span> <a href="/">Digital Marketing</a>
+//             </li>
+//           </ul>
+//         </div>
+        
+//         <div className='company'>
+//           <h4>Industries We Serve</h4>
+//           <ul className='ULlist'>
+//             <li className='list'>
+//               <span>&gt;</span> <a href="/">IT Industry</a>
+//             </li>
+//             <li className='list'>
+//               <span>&gt;</span> <a href="/">Healthcare Industry</a>
+//             </li>
+//             <li className='list'>
+//               <span>&gt;</span> <a href="/">Manufacturing Industry</a>
+//             </li>
+//             <li className='list'>
+//               <span>&gt;</span> <a href="/">Financial Services Industry</a>
+//             </li>
+//           </ul>
+//         </div>
+//       </div>
+
+//     </div>
+//       <div className='copyright'>
+//         <p className='content1'>&#169; All Rights Reserved | Powered By</p>
+//         <p className='content2'>&#160;MK DIGITALMARE PVT LTD</p>
+//       </div>
+//     </>
+//   )
+// }
+
+// export default Footer
+
+
+
 import React from 'react'
-import '../styles/Footer.css';
-import gamay from '../assets/gamay.png'
+import './Footer.scss'
+
 const Footer = () => {
   return (
     <>
-    <div className='Main'>
+      {/* <div className='Main'>
       <div className='details'>
         <div className='companyDetails'>
           <div className='logo'>
-            <img src={gamay} alt="" />
+            <img src="/images/gamay.png" alt="" />
           </div>
           <div className='address'>
             <p>GAMAYAS Plot No: 1,AP colony, Mallapur, Hyderabad PIN:-500076. <strong>Phone</strong>: +917801073175, +917801073176</p>
           </div>
         </div>
 
-        {/* company */}
         <div className='company'>
           <h4 style={{color: "white"}}>Company</h4>
           <ul className='ULlist'>
@@ -33,7 +118,7 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        {/* Our Focus */}
+
         <div className='company'>
           <h4>Our Focus Area</h4>
           <ul className='ULlist'>
@@ -75,7 +160,53 @@ const Footer = () => {
       <div className='copyright'>
         <p className='content1'>&#169; All Rights Reserved | Powered By</p>
         <p className='content2'>&#160;MK DIGITALMARE PVT LTD</p>
-      </div>
+      </div> */}
+      <footer class="footer">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-4">
+              <div class="logo">
+                <img src="/images/gamay.png" alt="" />
+              </div>
+              <div class="address">
+                <p>GAMAYAS Plot No: 1,AP colony, Mallapur, Hyderabad PIN:-500076. <strong>Phone</strong>: +917801073175, +917801073176</p>
+              </div>
+            </div>
+            <div class="col-md-2">
+              <h4 class="footer-heading">Company</h4>
+              <ul class="footer-list">
+                <li><a href="/">Home</a></li>
+                <li><a href="/">About Us</a></li>
+                <li><a href="/">Portfolio</a></li>
+                <li><a href="/">Contact Us</a></li>
+              </ul>
+            </div>
+            <div class="col-md-2">
+              <h4 class="footer-heading">Our Focus Area</h4>
+              <ul class="footer-list">
+                <li><a href="/">Web Design</a></li>
+                <li><a href="/">Web Development</a></li>
+                <li><a href="/">Mobile Apps Development</a></li>
+                <li><a href="/">Digital Marketing</a></li>
+              </ul>
+            </div>
+            <div class="col-md-2">
+              <h4 class="footer-heading">Industries We Serve</h4>
+              <ul class="footer-list">
+                <li><a href="/">IT Industry</a></li>
+                <li><a href="/">Healthcare Industry</a></li>
+                <li><a href="/">Manufacturing Industry</a></li>
+                <li><a href="/">Financial Services Industry</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="footer-bottom">
+          <p class="content1">&#169; All Rights Reserved | Powered By</p>
+          <p class="content2">&#160;MK DIGITALMARE PVT LTD</p>
+        </div>
+      </footer>
+
     </>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,120 +1,120 @@
 import logoImg from '../assets/logo.png';
-function Navbar() {
-    const logo = {
-      width: "182px",
-      height: "55px",
-      margin: "0 0 0 180px"
-    };
+// function Navbar() {
+//     const logo = {
+//       width: "182px",
+//       height: "55px",
+//       margin: "0 0 0 180px"
+//     };
   
-    const list = {
-      margin: "0 12px 0 12px"
-    }
+//     const list = {
+//       margin: "0 12px 0 12px"
+//     }
 
 
-    return (
-      <>
-        <nav style={{position: "absolute", width: "100%"}} className="navbar navbar-expand-lg navbar-dark shadow-5-strong " >
-          <div className="container-fluid  " >
-            <a className="navbar-brand " href="/">
-              <img style={logo} src={logoImg} alt="" />
-            </a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div id="navbarNavDropdown" style={{margin: "0 130px 0 0"}}>
-              <ul className="navbar-nav">
-                <li style={list} className="nav-item">
-                  <a className="nav-link fw-semibold text-white" aria-current="page" href="/">Home</a>
-                </li>
-                <li style={list} className="nav-item ">
-                  <a className="nav-link fw-semibold text-white " href="/">About us</a>
-                </li>
-                <li style={list} className="nav-item dropdown">
-                  <a className="nav-link dropdown-toggle fw-semibold text-white" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Services
-                  </a>
-                  <ul className="dropdown-menu">
-                    <li><a className="dropdown-item" href="/webdesign">Website Design Services</a></li>
-                    <li><a className="dropdown-item" href="/WebDevelopment">Website Development Services</a></li>
-                    <li><a className="dropdown-item" href="/ECommerceWebsiteDev">eCommerce Website Development</a></li>
-                    <li><a className="dropdown-item" href="/MobileAppDevelopment">Mobile App Development Company</a></li>
-                    <li><a className="dropdown-item" href="/DigitalMarketing">Digital Marketing</a></li>
-                    <li><a className="dropdown-item" href="/CMSDevelopment">CMS Development</a></li>
-                    <li><a className="dropdown-item" href="/CROServices">CRO Services</a></li>
-                    <li><a className="dropdown-item" href="/OutSourcing">Out Sourcing</a></li>
-                  </ul>
-                </li>
-                <li style={list} className="nav-item dropdown">
-                  <a className="nav-link dropdown-toggle fw-semibold text-white" href="/reales" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Industries
-                  </a>
-                  <ul
-                    class="dropdown-menu"
-                    aria-labelledby="navbarDropdownMenuLink"
-                  >
-                    <li>
-                      <a class="dropdown-item" href="/reales">
-                        Real E-State Industry
-                      </a>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="/education">
-                        Education industry
-                      </a>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="/retail">
-                        Retail Industry
-                      </a>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="/ecommerce">
-                        Ecommerce Industry
-                      </a>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="/itdomain">
-                        IT Industry
-                      </a>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="/healthcare">
-                        HealthCare Industry
-                      </a>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="/manufactoring">
-                        Manufactoring Industry
-                      </a>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="/financial">
-                        Financial Service Industry
-                      </a>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="/government">
-                        Government Industry
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-                <li style={list} className="nav-item">
-                  <a className="nav-link fw-semibold text-white" href="/">Portfolio</a>
-                </li>
-                <li style={list} className="nav-item">
-                  <a className="nav-link fw-semibold text-white" href="/">Contact Us</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
+//     return (
+//       <>
+//         <nav style={{position: "absolute", width: "100%"}} className="navbar navbar-expand-lg navbar-dark shadow-5-strong " >
+//           <div className="container-fluid  " >
+//             <a className="navbar-brand " href="/">
+//               <img style={logo} src={logoImg} alt="" />
+//             </a>
+//             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+//               <span className="navbar-toggler-icon"></span>
+//             </button>
+//             <div id="navbarNavDropdown" style={{margin: "0 130px 0 0"}}>
+//               <ul className="navbar-nav">
+//                 <li style={list} className="nav-item">
+//                   <a className="nav-link fw-semibold text-white" aria-current="page" href="/">Home</a>
+//                 </li>
+//                 <li style={list} className="nav-item ">
+//                   <a className="nav-link fw-semibold text-white " href="/">About us</a>
+//                 </li>
+//                 <li style={list} className="nav-item dropdown">
+//                   <a className="nav-link dropdown-toggle fw-semibold text-white" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+//                     Services
+//                   </a>
+//                   <ul className="dropdown-menu">
+//                     <li><a className="dropdown-item" href="/webdesign">Website Design Services</a></li>
+//                     <li><a className="dropdown-item" href="/WebDevelopment">Website Development Services</a></li>
+//                     <li><a className="dropdown-item" href="/ECommerceWebsiteDev">eCommerce Website Development</a></li>
+//                     <li><a className="dropdown-item" href="/MobileAppDevelopment">Mobile App Development Company</a></li>
+//                     <li><a className="dropdown-item" href="/DigitalMarketing">Digital Marketing</a></li>
+//                     <li><a className="dropdown-item" href="/CMSDevelopment">CMS Development</a></li>
+//                     <li><a className="dropdown-item" href="/CROServices">CRO Services</a></li>
+//                     <li><a className="dropdown-item" href="/OutSourcing">Out Sourcing</a></li>
+//                   </ul>
+//                 </li>
+//                 <li style={list} className="nav-item dropdown">
+//                   <a className="nav-link dropdown-toggle fw-semibold text-white" href="/reales" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+//                     Industries
+//                   </a>
+//                   <ul
+//                     class="dropdown-menu"
+//                     aria-labelledby="navbarDropdownMenuLink"
+//                   >
+//                     <li>
+//                       <a class="dropdown-item" href="/reales">
+//                         Real E-State Industry
+//                       </a>
+//                     </li>
+//                     <li>
+//                       <a class="dropdown-item" href="/education">
+//                         Education industry
+//                       </a>
+//                     </li>
+//                     <li>
+//                       <a class="dropdown-item" href="/retail">
+//                         Retail Industry
+//                       </a>
+//                     </li>
+//                     <li>
+//                       <a class="dropdown-item" href="/ecommerce">
+//                         Ecommerce Industry
+//                       </a>
+//                     </li>
+//                     <li>
+//                       <a class="dropdown-item" href="/itdomain">
+//                         IT Industry
+//                       </a>
+//                     </li>
+//                     <li>
+//                       <a class="dropdown-item" href="/healthcare">
+//                         HealthCare Industry
+//                       </a>
+//                     </li>
+//                     <li>
+//                       <a class="dropdown-item" href="/manufactoring">
+//                         Manufactoring Industry
+//                       </a>
+//                     </li>
+//                     <li>
+//                       <a class="dropdown-item" href="/financial">
+//                         Financial Service Industry
+//                       </a>
+//                     </li>
+//                     <li>
+//                       <a class="dropdown-item" href="/government">
+//                         Government Industry
+//                       </a>
+//                     </li>
+//                   </ul>
+//                 </li>
+//                 <li style={list} className="nav-item">
+//                   <a className="nav-link fw-semibold text-white" href="/">Portfolio</a>
+//                 </li>
+//                 <li style={list} className="nav-item">
+//                   <a className="nav-link fw-semibold text-white" href="/">Contact Us</a>
+//                 </li>
+//               </ul>
+//             </div>
+//           </div>
+//         </nav>
         
-      </>
-    );
-  }
+//       </>
+//     );
+//   }
   
-  export default Navbar;
+//   export default Navbar;
   
 
 // function Navbar() {
@@ -286,3 +286,148 @@ function Navbar() {
 //   }
   
 //   export default Navbar;    
+
+
+
+
+
+
+function Navbar() {
+
+  return (
+    <>
+
+      <nav
+        class="navbar navbar-expand-lg navbar-light bg-dark bg-opacity-10 mx=4"
+      >
+        <div className='webdesign1' class="container-fluid ">
+          <a class="navbar-brand " href="/">
+            <img style={{width: "182px"}} src="/images/logo.png" alt="" />
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon" style={{backgroundColor: "#a2a2a3", borderRadius: "5px"}}></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item mx-3">
+                <a class="nav-link active text-light " aria-current="page" href="/">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item mx-3">
+                <a class="nav-link text-light" href="/">
+                  About us
+                </a>
+              </li>
+              <li class="nav-item dropdown mx-3">
+                <a
+                  class="nav-link dropdown-toggle text-light"
+                  href="/webdesign"
+                  id="navbarDropdownMenuLink"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Services
+                </a>
+                <ul
+                  class="dropdown-menu"
+                  aria-labelledby="navbarDropdownMenuLink"
+                >
+                  <li>
+                    <a class="dropdown-item " href="/webdesign">
+                      Website Design Services
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="/WebDevelopment">
+                      Website Development Services
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="/ECommerceWebsiteDev">
+                      eCommerce Website Development
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="/MobileAppDevelopment">
+                      Mobile App Development Company
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="/DigitalMarketing">
+                      Digital Marketing
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="/CMSDevelopment">
+                      CMS Development
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="/CROServices">
+                      CRO Services
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="/OutSourcing">
+                      Out Sourcing
+                    </a>
+                  </li>
+                </ul>
+              </li>
+
+              <li class="nav-item dropdown mx-3">
+                <a
+                  class="nav-link dropdown-toggle text-light"
+                  href="/"
+                  id="navbarDropdownMenuLink"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Industries
+                </a>
+                <ul
+                  class="dropdown-menu"
+                  aria-labelledby="navbarDropdownMenuLink"
+                >
+                  <li>
+                    <a class="dropdown-item" href="/">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="/">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="/">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li class="nav-item mx-4">
+                <a class="nav-link text-light" href="/">
+                  Contact us
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
